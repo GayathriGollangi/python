2ceee9ce1085b68136b1ccf2a261624f463c4cf3
GayathriGollangi <gayathrigollangii@gmail.com>
2026-09-10 12:11:01 +0530

    Take a number from the user and find the sum of its digits.

--- operators/p4.py
+++ operators/p4.py
@@ -1,1 +1,14 @@
 # Take a number from the user and find the sum of its digits.
+nums = input()
+res = 0
+for num in nums:
+    res = res + int(num)
+print(res)
+
+# without using a string
+number = 123456
+res = 0
+while number>0:
+    res = res+number%10
+    number = number//10
+print(res)
